0aa8adf2a83e0504eddcf79807fce55061fb3c7b
Soufin Rahimeen <soufinrahimeen@gmail.com>
2015-01-31 22:31:21 -0500

    Base script

---
+++ dailytracker.py
@@ -0,0 +1,30 @@
+import praw
+import time
+
+r = praw.Reddit(user_agent = "Gets the Daily General Thread from subreddit.")
+print("Logging in...")
+r.login()
+
+words_to_match = ['daily general thread']
+
+cache = []
+
+def run_bot():
+    print("Grabbing subreddit...")
+    subreddit = r.get_subreddit("test")
+    print("Grabbing thread titles...")
+    threads = subreddit.get_hot(limit=20)
+    for submission in threads:
+        thread_title = submission.title.lower()
+        isMatch = any(string in thread_title for string in words_to_match)
+        if submission.id not in cache and isMatch:
+            print("Match found! Thread ID is " + submission.id)
+            r.send_message('FlameDra', 'DGT has been posted!', 'You are awesome!')
+            print("Message sent!")
+            cache.append(submission.id)
+    print("Comment loop finished. Restarting...")
+
+
+while True:
+    run_bot()
+    time.sleep(20)
